4b2fe72de851cf46eef3c4d38df2f997db53ae75
joseph strickland <jks2090@outlook.com>
2023-07-22 01:50:18 -0400

    updated services to add new features for match preferences

--- server.js
+++ server.js
@@ -52,7 +52,164 @@ const client = new MongoClient(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
+app.post('/matchPool', async (req, res) => {
+    admin.auth().verifyIdToken(req.body.token).then(async (decodedToken) => {
+        const client = new MongoClient(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+                await client.connect();
+                try {
+                    const collection = client.db("ropedin-mobile").collection("matchPreferences")
+                    let matchPreferences = await db.collection('matchPreferences').doc(req.body.uid).get()
+                    matchPreferences = matchPreferences.data()
+                    const query = {
+                        profileInvisible: { $eq: false },
+                        $and: [{ age: { $gte: matchPreferences.agesSelected.min } }, { age: { $lte: matchPreferences.agesSelected.max } }]
+                    }
+                    const orTypeQuery = { $or: [] }
+                    const orLevelQuery = { $or: [] }
+                    const orDaysQuery = { $or: [] }
+                    //only add to query if user wants one gender
+                    if (matchPreferences.partner.length == 1) {
+                        query.sex = { $eq: matchPreferences.partner[0] }
+                    }
+                    matchPreferences.level.forEach((level) => {
+                        orLevelQuery.$or.push({ level: level })
+                    })
+                    matchPreferences.climbingType.forEach((type) => {
+                        orTypeQuery.$or.push({ climbingType: type })
+                    })
+                    matchPreferences.daysSelected.forEach((day) => {
+                        orDaysQuery.$or.push({ daysSelected: day })
+                    })
+                    if (orLevelQuery.length) {
+                        query.$and.push(orLevelQuery)
+                    }
+                    if (orTypeQuery.length) {
+                        query.$and.push(orTypeQuery)
+                    }
+                    if (orDaysQuery.length) {
+                        query.$and.push(orDaysQuery)
+                    }
+                    let uids = []
+                    let rejectedUsersList = await db.collection('rejects').doc(req.body.uid).get()
+                    if (typeof rejectedUsersList.data() !== 'undefined' && rejectedUsersList.data().rejectUIDs) {
+                        rejectedUsersList.data().rejectUIDs.forEach((uid) => { uids.push(uid) })
+                    }
+                    let blockedUsers = await db.collection('blockedLists').doc(req.body.uid).get()
+                    if (typeof blockedUsers.data() !== 'undefined' && blockedUsers.data().blockedUIDs) {
+                        blockedUsers.data().blockedUIDs.forEach((uid) => uids.push(uid))
+                    }
+                    let alreadyMatchedUsers = await db.collection('matches').doc(req.body.uid).get()
+                    if (alreadyMatchedUsers.data().matchUIDs) {
+                        alreadyMatchedUsers.data().matchUIDs.forEach((uid) => uids.push(uid))
+                    }
+                    let alreadySwipedUsers = await db.collection('users').doc(req.body.uid).collection('matchPool').doc('potentialMatches').get()
+                    if (typeof alreadySwipedUsers.data() !== 'undefined' && alreadySwipedUsers.data().matchUIDs) {
+                        alreadySwipedUsers.data().matchUIDs.forEach((uid) => uids.push(uid))
+                    }
+                    uids.push(req.body.uid)
+                    query.$and.push({ uid: { $nin: uids } })
+                    // limit query results for scalability
+                    cursor = collection.find(query)
+                    let matchUIDs = []
+                    await cursor.forEach(function (cursorItem) {
+                        matchUIDs.push(cursorItem.uid);
+                    });
+                    // while (await cursor.hasNext()) {
 
+                    //   doc = await cursor.next()
+
+                    //   matchUIDs.push(doc.uid)
+
+                    // }
+                    cursor.close()
+                    miles = 60
+                    meters = miles * 1609.34
+                    radius = meters
+                    const geoQuery = {
+                        "location": {
+                            $near: {
+                                $geometry: {
+                                    type: "Point",
+                                    coordinates: [req.body.location.longitude, req.body.location.latitude]
+                                }, $maxDistance: radius
+                            }
+                        },
+                        uid: { $in: matchUIDs }
+                    }
+                    const geoCollection = client.db("ropedin-mobile").collection("geoDocuments")
+                    geoCursor = geoCollection.find(geoQuery).limit(50)
+                    let newMatchUIDs = []
+                    await geoCursor.forEach(function (cursorItem) {
+                        newMatchUIDs.push(cursorItem.uid);
+                    });
+                    // while (await geoCursor.hasNext()) {
+
+                    //   doc = await geoCursor.next()
+
+                    //   newMatchUIDs.push(doc.uid)
+
+                    // }
+                    geoCursor.close()
+                    const matchPool = []
+                    await Promise.all(newMatchUIDs.map(async (matchUID) => {
+                        let matchProfile = await db.collection('users').doc(matchUID).get()
+                        if (matchProfile.data()) {
+                            matchProfile = matchProfile.data().profile
+                            matchPool.push(matchProfile)
+                        }
+                    }))
+                    //only cache match pool if it has potential matches in it
+                    if (matchPool.length) {
+                        //cache matchPool in DB; don't run this function fully until match pool exhausted or expired; store in separate collection/doc so that the data don't get tacked onto any profile data retrievals
+                        await db.collection('users').doc(req.body.uid).collection('matchPool').doc('cachedPool').set({ createdAt: new Date(), matchPoolUIDs: newMatchUIDs })
+                    }
+                    res.status = 200
+                    res.send({ matchPool: matchPool })
+                    client.close()
+                }
+                catch (err) {
+                    console.log("Match pool error: " + err)
+                    res.status = 500
+                    res.send([])
+                    client.close()
+                }
+    }).catch((err) => {
+        console.log("Match pool error: " + err)
+        res.status = 403
+        res.send([])
+        client.close()
+    })
+})
+app.post('/matchPreferences', async (req, res) => {
+
+    admin.auth().verifyIdToken(req.body.token).then(async (decodedToken) => {
+  
+      const client = new MongoClient(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+      await client.connect();
+    //   client.connect(async err => {
+  
+    //     if (err) {
+    //       res.sendStatus(500)
+    //       return
+    //     }
+  
+        const collection = client.db("ropedin-mobile").collection("matchPreferences")
+  
+        collection.updateOne({ uid: decodedToken.uid }, { $set: req.body }, (err, res) => {
+  
+          client.close()
+  
+        })
+  
+        res.sendStatus(200)
+  
+      })
+    // })
+    //   .catch((err) => {
+    //     console.log("Error updating match preferences: " + err)
+    //     res.sendStatus(403)
+    //   })
+  })
 app.post('/createUser', async (req, res) => {
     try {
         admin.auth().verifyIdToken(req.body.token).then(async (decodedToken) => {
